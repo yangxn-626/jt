bd0aff72f53e9f99a52f5c8ae83c49246622183a
yangxn-626 <18369903959@163.com>
2021-05-17 22:38:23 +0800

    init

--- jt-manage/src/main/java/com/jt/SpringBootRun.java
+++ jt-manage/src/main/java/com/jt/SpringBootRun.java
@@ -3,14 +3,11 @@ package com.jt;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-ss
+
 @SpringBootApplication
 @MapperScan("com.jt.mapper")
 public class SpringBootRun {
-	xz
 	public static void main(String[] args) {
-		
 		SpringApplication.run(SpringBootRun.class, args);
 	}
 }
-wqe
